30500931c9de76c5bcdac1a45fd83c41431e6ace
jlstout1998 <jlstout1998@gmail.com>
2026-06-18 21:36:18 -0700

    Update ModMenuIntegration.java

--- src/main/java/dev/caoimhe/compactchat/modmenu/ModMenuIntegration.java
+++ src/main/java/dev/caoimhe/compactchat/modmenu/ModMenuIntegration.java
@@ -6,11 +6,12 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.caoimhe.compactchat.config.Configuration;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(Configuration.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(Configuration.class, parent).get();
     }
 }
 
