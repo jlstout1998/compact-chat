f86d8cf88dfae2652a1903e2d7f18ace15be07e2
jlstout1998 <jlstout1998@gmail.com>
2026-06-18 22:16:17 -0700

    Modify addClientSystemMessage method signature

--- src/main/java/dev/caoimhe/compactchat/mixin/ChatHudMixin.java
+++ src/main/java/dev/caoimhe/compactchat/mixin/ChatHudMixin.java
@@ -33,7 +33,7 @@ public abstract class ChatHudMixin implements IChatHudExt {
     private final MessageManager messageManager = new MessageManager(this);
 
     @ModifyVariable(
-        method = "addClientSystemMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;Lnet/minecraft/client/multiplayer/chat/GuiMessageTag;)V",
+        method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;Lnet/minecraft/client/multiplayer/chat/GuiMessageSource;Lnet/minecraft/client/multiplayer/chat/GuiMessageTag;)V",
         at = @At("HEAD"),
         argsOnly = true
     )
