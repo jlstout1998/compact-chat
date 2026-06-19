4197728c1abf6b9eae4f4288f63c63926a865eb0
jlstout1998 <jlstout1998@gmail.com>
2026-06-18 23:26:40 -0700

    Update NeoForge version in root.gradle.kts

--- root.gradle.kts
+++ root.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 preprocess {
     val fabric262 = createNode("26.2-fabric", 26_02_00, "official")
-    val neoforge262 = createNode("26.2-neoforge", 26_02_00, "official")
+    val neoforge262 = createNode("1.21.8-neoforge", 26_02_00, "official")
 
     neoforge262.link(fabric262)    // Fabric 26.2    ->  NeoForge 26.2
 }
