33462817cdf38c5aecd2de93dce1e0543d3c41a7
jlstout1998 <jlstout1998@gmail.com>
2026-06-18 19:41:23 -0700

    Update Loom version and add Fabric 26.2 nodes
    Updated Loom plugin version and added new fabric and neoforge nodes.

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,12 +1,12 @@
 // https://github.com/EssentialGG/essential-gradle-toolkit
 plugins {
-    id("gg.essential.loom") version "1.9.32" apply false
+    id("gg.essential.loom") version "1.21.8" apply false
     id("gg.essential.multi-version.root")
 }
 
 preprocess {
-    val fabric12101 = createNode("1.21.1-fabric", 12101, "yarn")
-    val neoforge12101 = createNode("1.21.1-neoforge", 12101, "official")
+    val fabric262 = createNode("26.2-fabric", 262, "official")
+    val neoforge262 = createNode("26.2-neoforge", 262, "official")
 
-    neoforge12101.link(fabric12101)    // Fabric 1.21.1    ->  NeoForge 1.21.1
+    neoforge262.link(fabric262)    // Fabric 26.2    ->  NeoForge 26.2
 }
