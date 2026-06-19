09294a01c531d534b2e9e365b8defd15bd6867a1
jlstout1998 <jlstout1998@gmail.com>
2026-06-18 20:41:57 -0700

    Update root.gradle.kts

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,12 +1,12 @@
 // https://github.com/EssentialGG/essential-gradle-toolkit
 plugins {
-    id("gg.essential.loom") version "1.21.8" apply false
+    // id("gg.essential.loom") version "1.21.8" apply false
     id("gg.essential.multi-version.root")
 }
 
 preprocess {
-    val fabric262 = createNode("26.2-fabric", 262, "official")
-    val neoforge262 = createNode("26.2-neoforge", 262, "official")
+    val fabric262 = createNode("26.2-fabric", 26_02_00, "official")
+    val neoforge262 = createNode("26.2-neoforge", 26_02_00, "official")
 
     neoforge262.link(fabric262)    // Fabric 26.2    ->  NeoForge 26.2
 }
