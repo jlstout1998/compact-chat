c2db953aa388a61efe36335569b855d1264588dc
jlstout1998 <jlstout1998@gmail.com>
2026-07-01 21:45:54 -0700

    Add '26.2-neoforge' to project versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,7 @@ rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
     "26.2-fabric",
+    "26.2-neoforge",
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
